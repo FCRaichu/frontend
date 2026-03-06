0ac873d10c98d492330b2084e99707a6ef538e67
kye1115z <kye1115z@naver.com>
2026-03-06 16:28:27 +0900

    #2 setting: 절대 경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
+// DONE:  절대경로 설정하기
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
-// TODO:  절대경로 설정하기
+import path from "path"; // path 모듈 불러오기
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [
-      //
-    ],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@apis": path.resolve(__dirname, "./src/apis"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@data": path.resolve(__dirname, "./src/data"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@layouts": path.resolve(__dirname, "./src/layouts"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@stores": path.resolve(__dirname, "./src/stores"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      "@types": path.resolve(__dirname, "./src/types"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+    },
   },
 });
