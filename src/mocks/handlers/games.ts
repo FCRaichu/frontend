14b9ad64c40a375e6baaf33ee55b614811783cc8
YeEun Kim <kye1115z@naver.com>
2026-03-09 02:22:34 +0900

    #2 feat: 경기 일자 api 받아오기 위한 mocking 적용 (데이터, 핸들러 추가)

---
+++ src/mocks/handlers/games.ts
@@ -0,0 +1,12 @@
+import { http, HttpResponse } from "msw";
+import { mockScheduleResponse } from "../data/play";
+
+// 경기 관련 mocking 함수 모아두기
+export const gamesHandler = [
+  // 1. 경기 일정 Get
+  // TODO: 실제 서버 API로 수정
+  http.get("/api/games", async () => {
+    // 경기 일정 데이터 반환
+    return HttpResponse.json(mockScheduleResponse, { status: 200 });
+  }),
+];
--- src/mocks/handlers/handlers.ts
+++ src/mocks/handlers/handlers.ts
@@ -1,4 +1,5 @@
 import { signUpHandler } from "./auth";
+import { gamesHandler } from "./games";
 
 // 전체 mocking 함수 관리
-export const handlers = [...signUpHandler];
+export const handlers = [...signUpHandler, ...gamesHandler];
