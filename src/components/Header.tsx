eb177045e7149797432b5755e1d90b2260def5e9
YeEun Kim <kye1115z@naver.com>
2026-03-09 00:47:58 +0900

    #2 feat: Header 컴포넌트 생성 및 store 연결

---
+++ src/components/Header.tsx
@@ -0,0 +1,41 @@
+import { useAuthStore } from "@/stores/useAuthStore";
+import { Link, NavLink } from "react-router-dom";
+
+export default function Header() {
+  // 전역 store에서 user 가져오기 (콕 찝어서 가져와야 다른 상태가 바뀌었을 때 리렌더링 안 됨.)
+  const { user } = useAuthStore();
+  //   const navigation = useNavigate();
+
+  return (
+    <header>
+      <nav>
+        <Link to="/" className="logo">
+          logo
+        </Link>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/post">Post</NavLink>
+          </li>
+          <li>
+            <NavLink to="/donation">Donation</NavLink>
+          </li>
+        </ul>
+        <div>
+          {user ? (
+            <span>
+              환영합니다, <b>{user.nickname}님</b>
+            </span>
+          ) : (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Join Us</Link>
+            </>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+}
