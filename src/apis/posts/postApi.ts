4bb087e2f674c5a18f4134d7c0483a37a2c27cb6
kye1115z <kye1115z@naver.com>
2026-03-13 17:54:03 +0900

    #22 직관 기록 작성 전송 시 필요한 로직 및 타입 설정

--- src/apis/posts/postApi.ts
+++ src/apis/posts/postApi.ts
@@ -1,7 +1,14 @@
+import type { PostRequest } from "@/types/post";
 import { api } from "../axiosInstance";
 
 // 직관 기록 전체 조회
 export const getMyAllRecords = async () => {
   const res = await api.get(`/api/posts`);
+  return res;
+};
+
+// 직관 기록 작성
+export const postMyRecord = async (data: PostRequest) => {
+  const res = await api.post(`/api/posts`, data);
   return res;
 };
