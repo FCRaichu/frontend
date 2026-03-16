32e053f2a73713e91d198abf2c8de55d0086d790
kye1115z <kye1115z@naver.com>
2026-03-16 15:26:48 +0900

    #26 fix: 배포 시 랭킹 데이터 안 나오는 문제 해결

--- src/apis/ranking/rankApi.ts
+++ src/apis/ranking/rankApi.ts
@@ -4,11 +4,15 @@ const currentYear = new Date().getFullYear();
 
 // 직관왕
 export const getAttendanceRanking = async () => {
-  const res = await axios.get(`/api/ranking/attendance?year=${currentYear}`);
+  const res = await axios.get(
+    `/api/rankings/attendance?year=${currentYear.toString()}`,
+  );
   return res.data;
 };
 // 직관왕
 export const getWinRateRanking = async () => {
-  const res = await axios.get(`/api/ranking/win-rate?year=${currentYear}`);
+  const res = await axios.get(
+    `/api/rankings/win-rate?year=${currentYear.toString()}`,
+  );
   return res.data;
 };
