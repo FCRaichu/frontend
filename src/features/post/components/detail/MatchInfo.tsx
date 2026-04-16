85b5dd06f180e1e1f49903f96467cb981e31005d
yekim <kye1115z@naver.com>
2026-04-16 09:39:59 +0900

    #42 style: 직관 상세 경기 정보에서 점수 보여주기

--- src/features/post/components/detail/MatchInfo.tsx
+++ src/features/post/components/detail/MatchInfo.tsx
@@ -1,52 +1,93 @@
 import React from "react";
 import Typography from "@/components/common/Typography";
-import {
-  MdOutlineSportsSoccer,
-  MdOutlineCalendarToday,
-  MdOutlineLocationOn,
-} from "react-icons/md";
+import FCSeoulLogo from "@/assets/fcseoul_logo.png";
+import { FC_TEAMS } from "@/data/fc_teams";
+import { formatGameDateTime } from "@/utils/formatDate";
 
 interface MatchInfoProps {
   date: string;
   opponent: string;
   stadium: string;
+  result: [number, number];
 }
 
-const MatchInfo: React.FC<MatchInfoProps> = ({ date, opponent, stadium }) => {
+const MatchInfo: React.FC<MatchInfoProps> = ({
+  date,
+  opponent,
+  stadium,
+  result,
+}) => {
+  const { formattedDate, formattedTime } = formatGameDateTime(date);
+
+  const opponentData = (opponent: string) => {
+    return FC_TEAMS.find(
+      (t) => t.team.includes(opponent) || t.location.includes(opponent),
+    );
+  };
+
   return (
-    <div className="inline-block bg-background p-6 rounded-xl border border-background shadow-sm w-full">
-      <div className="flex items-center gap-2 mb-4">
-        <span className="bg-red-50 text-primary text-[10px] font-bold px-1.5 py-0.5 rounded border border-red-100">
-          K LEAGUE 1
-        </span>
+    <div className="flex justify-between items-center px-24 py-10 bg-background border-b-2 border-subtleGray">
+      {/* Home Team (FC 서울) */}
+      <div className="flex flex-col items-center gap-3 z-10">
+        <div className="flex items-center justify-center">
+          <img
+            src={FCSeoulLogo}
+            alt="fc seoul logo"
+            className="h-24 object-contain"
+          />
+        </div>
+        <Typography variant="body-sm" color="text-disabledGray">
+          FC 서울
+        </Typography>
       </div>
 
-      <div className="space-y-2">
+      {/* 경기 정보 */}
+      <div className="flex w-full justify-between items-center px-16">
         <Typography
-          variant="h4"
-          color="text-textMain"
-          className="flex items-center gap-3 font-bold!"
-        >
-          <MdOutlineCalendarToday className="text-primary text-xl" />
-          {date}
+          variant="display"
+          color="text-secondary"
+          className="font-bold! italic">
+          {result[0]}
         </Typography>
-
+        <div className="flex flex-col items-center gap-2">
+          <Typography
+            variant="body-xs"
+            color="text-disabledGray"
+            className="px-3 py-1 rounded-full border">
+            {stadium}
+          </Typography>
+          <Typography
+            variant="h2"
+            color="text-secondary"
+            className="font-extrabold">
+            {formattedDate}
+          </Typography>
+          <Typography variant="body-sm" color="text-disabledGray">
+            {formattedTime}
+          </Typography>
+        </div>
         <Typography
-          variant="h4"
-          color="text-textMain"
-          className="flex items-center gap-3 font-bold!"
-        >
-          <MdOutlineSportsSoccer className="text-primary text-xl" />
-          FC서울 VS {opponent}
+          variant="display"
+          color="text-secondary"
+          className="font-bold! italic">
+          {result[1]}
         </Typography>
+      </div>
 
+      {/* 상대 팀 */}
+      <div className="flex flex-col items-center gap-3 z-10">
+        <div className="flex items-center justeify-center">
+          <img
+            src={opponentData(opponent)?.image}
+            alt={`${opponent} logo`}
+            className="h-24 object-contain"
+          />
+        </div>
         <Typography
           variant="body-sm"
-          color="text-textSub"
-          className="flex items-center gap-3 font-medium!"
-        >
-          <MdOutlineLocationOn className="text-primary text-xl" />
-          {stadium}
+          color="text-disabledGray"
+          className="whitespace-nowrap">
+          {opponentData(opponent)?.team}
         </Typography>
       </div>
     </div>
