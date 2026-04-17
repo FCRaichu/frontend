23b1f5a60ca0c2c2a64ed90c9f3f8ef0978104d0
yekim <kye1115z@naver.com>
2026-04-17 15:56:13 +0900

    #42 feat: Login 및 닉네임 설정 로직 변경

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,93 +1,42 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-
-import { getLogin } from "@/features/auth/api/authApi";
-import LogoAnimation from "@/features/auth/components/login/LogoAnimation";
-import Typography from "@/components/common/Typography";
-import Button from "@/components/common/Button";
-import Input from "@/components/common/Input";
+import {
+  handleAuthCallback,
+  loginWithKeycloak,
+} from "@/features/auth/api/authApi";
+import { useEffect } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 export default function Login() {
+  const [searchParams] = useSearchParams();
   const navigate = useNavigate();
 
-  // 로그인 시 필요한 데이터 정의
-  const [loginData, setLoginData] = useState({
-    userId: "",
-    password: "",
-  });
+  useEffect(() => {
+    const processAuth = async () => {
+      const code = searchParams.get("code");
+      const state = searchParams.get("state");
 
-  // 키값을 이벤트 객체로 가져와서 데이터 set 하기
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setLoginData({
-      ...loginData,
-      [e.target.name]: e.target.value,
-    });
-  };
+      if (code && state) {
+        try {
+          const result = await handleAuthCallback(code, state);
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    try {
-      await getLogin(loginData);
-      alert("로그인 성공!");
-      navigate("/");
-    } catch (e) {
-      console.error("로그인 실패: ", e);
-      alert("로그인에 실패했습니다. 아이디와 비밀번호를 확인해주세요.");
-    }
-  };
+          if (result.status === "SUCCESS") {
+            // 기존 회원은 로그인 성공 및 메인 페이지로 이동
+            navigate("/");
+          } else if (result.status === "NEED_SIGNUP") {
+            // 미가입 사용자는 닉네임 설정 페이지로 이동
+            navigate("/signup");
+          }
+        } catch (e) {}
+      } else {
+        // 최초 진입 시, keycloak 로그인 페이지로 리다이렉트
+        loginWithKeycloak();
+      }
+    };
 
+    processAuth();
+  }, []);
   return (
-    <div className="bg-secondary box-border overflow-x-hidden min-h-screen flex flex-col items-center justify-center pt-29 pb-10">
-      <form
-        onSubmit={handleSubmit}
-        className="flex flex-col items-center w-full h-full"
-      >
-        <LogoAnimation />
-        <div className="flex flex-col items-center mb-8">
-          <Typography variant="h1" color="text-background" className="mb-4">
-            로그인
-          </Typography>
-          <Typography variant="h4" color="text-disabledGray">
-            My Fc Seoul 회원 로그인
-          </Typography>
-        </div>
-
-        <div className="flex flex-col items-start w-125">
-          <div className="flex flex-col gap-8 w-full">
-            <Input
-              label="ID"
-              name="userId"
-              placeholder="아이디"
-              onChange={handleChange}
-            />
-            <Input
-              label="비밀번호"
-              name="password"
-              placeholder="비밀번호"
-              type="password"
-              onChange={handleChange}
-            />
-          </div>
-
-          <div className="flex justify-between items-end w-full mt-4">
-            <Button type="submit" width="fixed" className="mt-8 mr-8">
-              로그인
-            </Button>
-            <Link
-              to="/signup"
-              className="flex gap-2 hover:opacity-80 transition-opacity"
-            >
-              <Typography
-                variant="body-md"
-                color="text-textSub"
-                className="hover:underline hover:text-background transition-colors "
-              >
-                회원가입
-              </Typography>
-            </Link>
-          </div>
-        </div>
-      </form>
+    <div className="flex items-center justify-center h-screen">
+      <p className="text-body-lg">로그인 페이지로 이동 중입니다...</p>
     </div>
   );
 }
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -8,79 +8,48 @@ import Input from "@/components/common/Input";
 export default function SignUp() {
   const navigate = useNavigate();
 
-  const [signUpData, setSignUpData] = useState({
-    userId: "",
-    password: "",
-    nickname: "",
-  });
+  const [nickname, setNickname] = useState("");
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSignUpData({
-      ...signUpData,
-      [e.target.name]: e.target.value,
-    });
+    setNickname(e.target.value);
   };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const res = await postSingUp(signUpData);
+      const res = await postSingUp(nickname);
       if (res.status === 201) {
-        alert("가입을 환영합니다! 로그인을 진행해 주세요.");
-        navigate("/login");
+        alert("가입을 환영합니다!");
+        navigate("/");
       }
     } catch (error) {
-      console.error("회원가입 실패: ", error);
+      console.error("닉네임 설정 실패: ", error);
     }
   };
 
   return (
     <form
       onSubmit={handleSubmit}
-      className="flex flex-col items-center pt-24 md:pt-40 px-6 w-full min-h-screen bg-secondary box-border overflow-x-hidden"
-    >
+      className="flex flex-col items-center pt-24 md:pt-40 px-6 w-full bg-secondary box-border overflow-x-hidden">
       <div className="flex flex-col items-center mb-12 md:mb-16 text-center">
         <Typography
           variant="h1"
           color="text-background"
-          className="mb-4 text-3xl md:text-4xl"
-        >
-          회원 가입
+          className="mb-4 text-3xl md:text-4xl">
+          닉네임 설정
         </Typography>
         <Typography variant="h4" color="text-disabledGray">
-          My FC Seoul 회원가입
+          닉네임은 초기 설정 이후 변경이 가능합니다.
         </Typography>
       </div>
 
-      <div className="flex flex-col items-start w-full max-w-100 md:max-w-125">
-        <div className="flex flex-col gap-6 md:gap-8 w-full">
-          <Input
-            label="ID"
-            name="userId"
-            placeholder="아이디"
-            onChange={handleChange}
-          />
-          <Input
-            label="비밀번호"
-            name="password"
-            placeholder="비밀번호"
-            type="password"
-            onChange={handleChange}
-          />
-          <Input
-            label="닉네임"
-            name="nickname"
-            placeholder="닉네임"
-            onChange={handleChange}
-          />
-        </div>
-
+      <div className="flex items-center justify-center w-full max-w-100 md:max-w-125 gap-3">
+        <Input name="nickname" placeholder="수호신" onChange={handleChange} />
         <Button
           type="submit"
           width="fixed"
-          className="mt-12 md:mt-16 w-full md:w-auto self-center md:self-start"
-        >
-          가입하기
+          className="w-12 self-center md:self-end">
+          확인
         </Button>
       </div>
     </form>
