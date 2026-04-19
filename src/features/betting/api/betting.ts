d093a62d4b623e23b40a10fad09961b18e4e74c4
yekim <kye1115z@naver.com>
2026-04-19 15:37:35 +0900

    #42: 배팅 API 정의

--- src/features/betting/api/betting.ts
+++ src/features/betting/api/betting.ts
@@ -1,14 +1,38 @@
 import { api } from "@api/axiosInstance";
+import type { PostBettingRequest } from "../types/betting";
 
-// GET: 배팅 가능한 경기 정보 불러오기
+// GET: 현재 배팅 중인 정보 가져오기
 export const getActiveBettingGames = async () => {
   const res = await api.get(`/api/bet`);
-  console.log(res.data);
   return res.data;
 };
 
-// donation
-export const postDonation = async (playerId: number, points: number) => {
-  const res = await api.post(`/api/donation/${playerId}?point=${points}`);
+// POST: 배팅하기
+export const postBetting = async (betData: PostBettingRequest) => {
+  const res = await api.post(`/api/bet`, betData);
+  return res.data;
+};
+
+// GET: 이전 나의 배팅 기록들
+export const getMyBettingHistory = async () => {
+  const res = await api.get(`api/bet/history`);
+  return res.data;
+};
+
+// GET: 나의 배팅 전적
+export const getMyBettingStats = async () => {
+  const res = await api.get(`api/bet/record`);
+  return res.data;
+};
+
+// GET: 확인하지 않은 정산 결과 있으면 로그인 후 PUT 호출
+export const getMyUnreadBetting = async () => {
+  const res = await api.get(`api/bet/unread`);
+  return res.data;
+};
+
+// PUT: 확인하지 않은 배팅 정산 결과를 확인 처리
+export const putMyUnreadBetting = async (Ids: number[]) => {
+  const res = await api.put(`api/bet/unread/check`, { Ids });
   return res.data;
 };
