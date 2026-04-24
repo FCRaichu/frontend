612e7a0eefcb3ee5289f168c12c553d80b9d1525
yekim <kye1115z@naver.com>
2026-04-24 14:45:59 +0900

    ⭐️ feat: 로그아웃을 위해 백엔드에서 logout api 호출 및 keycloak SSO 세션 종료 리다이렉트

--- src/stores/useAuthStore.ts
+++ src/stores/useAuthStore.ts
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import type { User } from "@/features/auth/types/auth";
+import { api } from "@/api/axiosInstance";
 
 // 유저 상태
 interface AuthState {
@@ -20,7 +21,7 @@ interface AuthState {
   setUser: (user: User) => void;
   updateUser: (userInfo: Partial<User>) => void;
   isLoggedIn: () => boolean; // 로그인 여부 확인
-  logout: () => void; // 로그아웃 시 호출
+  logout: () => Promise<void>; // 로그아웃 시 호출
 }
 
 // zustand 스토어 생성
@@ -64,16 +65,46 @@ export const useAuthStore = create<AuthState>()(
 
       isLoggedIn: () => !!get().accessToken, // 로그인 여부 확인
 
-      // 로그아웃: 모든 인증 정보를 초기화
-      logout: () =>
-        set({
-          user: null,
-          accessToken: null,
-          tokenType: null,
-          refreshToken: null,
-        }),
-    }),
+      // 로그아웃
+      logout: async () => {
+        try {
+          // 1. 백엔드에 로그아웃 요청
+          const res = await api.get("/api/auth/logout");
+          const { logoutUrl } = res.data;
 
-    { name: "auth-storage" }, // 로컬 스토리지에 저장될 키 이름
+          // 2. 로컬 상태 정리
+          // Zustand 및 로컬 스토리지 상태 초기화
+          set({
+            user: null,
+            accessToken: null,
+            tokenType: null,
+            refreshToken: null,
+          });
+
+          localStorage.clear();
+
+          // 3. Keycloak SS0 세션 종료를 위해 리다이렉트
+          // Keycloak 서버로 이동하여 세션을 강제로 끊고 다시 프론트엔드로 리다이렉트
+          if (logoutUrl) {
+            window.location.href = logoutUrl;
+          } else {
+            // URL을 못 받았을 경우 예외 처리
+            window.location.href = "/";
+          }
+        } catch (error) {
+          console.error("로그아웃 처리 중 에러 발생:", error);
+
+          // 에러가 발생해도 로컬 상태는 초기화
+          set({
+            user: null,
+            accessToken: null,
+            tokenType: null,
+            refreshToken: null,
+          });
+          // window.location.href = "/";
+        }
+      },
+    }),
+    { name: "auth-storage" },
   ),
 );
